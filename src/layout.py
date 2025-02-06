3e49142f8fc8c1f34e4684176670a2fe6d10d554
yilinxia <xia.yl@outlook.com>
2025-02-06 15:52:16 -0600

    hook succeed

--- src/layout.py
+++ src/layout.py
@@ -412,7 +412,7 @@ layout = html.Div(
                                         className="me-3",
                                     ),  # Logo Placeholder
                                     html.Span(
-                                        "WESE - Render Hook Test",
+                                        "WESE",
                                         style={
                                             "font-weight": "bold",
                                             "color": "#333333",
