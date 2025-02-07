06d73d9a466b41606383ac1afe9e216a60d0751b
Yilin Xia <xia.yl@outlook.com>
2025-02-07 11:43:37 -0600

    fix the auto-cleaner

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src"
 
 from server import server
 from layout import layout
+import callbacks  # noqa: F401 (necessary for callbacks to register)
+
 
 app = dash.Dash(
     __name__,
