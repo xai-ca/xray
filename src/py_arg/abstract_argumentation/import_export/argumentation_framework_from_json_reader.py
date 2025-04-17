2fa08c33bff2717ebce743600a57e12243929633
Yilin Xia <xia.yl@outlook.com>
2025-04-17 13:23:19 -0500

    change the json structure

--- src/py_arg/abstract_argumentation/import_export/argumentation_framework_from_json_reader.py
+++ src/py_arg/abstract_argumentation/import_export/argumentation_framework_from_json_reader.py
@@ -6,23 +6,63 @@ from py_arg.abstract_argumentation.classes.abstract_argumentation_framework \
     import AbstractArgumentationFramework
 
 
+# class ArgumentationFrameworkFromJsonReader:
+#     def __init__(self):
+#         pass
+
+#     @staticmethod
+#     def from_json(json_object) -> AbstractArgumentationFramework:
+#         if json_object['name']:
+#             name = json_object['name']
+#         else:
+#             name = ''
+#         arguments = [Argument(argument_name)
+#                      for argument_name in json_object['arguments']]
+#         defeats = [Defeat(Argument(from_argument), Argument(to_argument)) for
+#                    from_argument, to_argument in json_object['defeats']]
+#         return AbstractArgumentationFramework(name, arguments, defeats)
+
+#     def read_from_json(self, file_path: str) -> AbstractArgumentationFramework:
+#         with open(file_path, 'r') as reader:
+#             argumentation_framework_json = json.load(reader)
+#         return self.from_json(argumentation_framework_json)
+
+
 class ArgumentationFrameworkFromJsonReader:
     def __init__(self):
         pass
 
     @staticmethod
     def from_json(json_object) -> AbstractArgumentationFramework:
-        if json_object['name']:
-            name = json_object['name']
-        else:
-            name = ''
-        arguments = [Argument(argument_name)
-                     for argument_name in json_object['arguments']]
-        defeats = [Defeat(Argument(from_argument), Argument(to_argument)) for
-                   from_argument, to_argument in json_object['defeats']]
-        return AbstractArgumentationFramework(name, arguments, defeats)
+        # -- 1) Name
+        name = json_object.get('name', '')
+
+        # -- 2) Build Argument instances (and keep them in a dict for lookup)
+        arg_map = {}
+        for a in json_object.get('arguments', []):
+            arg_id = a['id']
+            arg_obj = Argument(arg_id)
+            # attach your extra fields
+            # arg_obj.annotation = a['annotation']
+            # arg_obj.url        = a['url']
+            arg_map[arg_id] = arg_obj
+
+        # -- 3) Build Defeat instances
+        defeats = []
+        for d in json_object.get('defeats', []):
+            attacker = arg_map[d['from']]
+            target   = arg_map[d['to']]
+            defeat = Defeat(attacker, target)
+            # attach the defeat’s annotation string
+            # defeat.annotation = d['annotation']
+            defeats.append(defeat)
+
+        # -- 4) Return the framework
+        return AbstractArgumentationFramework(name,
+                                              list(arg_map.values()),
+                                              defeats)
 
     def read_from_json(self, file_path: str) -> AbstractArgumentationFramework:
         with open(file_path, 'r') as reader:
-            argumentation_framework_json = json.load(reader)
-        return self.from_json(argumentation_framework_json)
+            af_json = json.load(reader)
+        return self.from_json(af_json)
--- src/py_arg/abstract_argumentation/import_export/argumentation_framework_to_json_writer.py
+++ src/py_arg/abstract_argumentation/import_export/argumentation_framework_to_json_writer.py
@@ -5,23 +5,65 @@ from py_arg.abstract_argumentation.classes.abstract_argumentation_framework \
 from py_arg.incomplete_aspic.import_export.writer import Writer
 
 
+# class ArgumentationFrameworkToJSONWriter(Writer):
+#     def __init__(self):
+#         super().__init__()
+
+#     @staticmethod
+#     def to_dict(argumentation_framework: AbstractArgumentationFramework):
+#         return {'name': argumentation_framework.name,
+#                 'arguments':
+#                     [str(argument)
+#                      for argument in argumentation_framework.arguments],
+#                 'defeats': [(str(defeat.from_argument),
+#                              str(defeat.to_argument))
+#                             for defeat in argumentation_framework.defeats]}
+
+#     def write(self, argumentation_framework: AbstractArgumentationFramework,
+#               file_name: str):
+#         write_path = self.data_folder / file_name
+#         result = self.to_dict(argumentation_framework)
+#         with open(write_path, 'w') as write_file:
+#             json.dump(result, write_file)
+
 class ArgumentationFrameworkToJSONWriter(Writer):
     def __init__(self):
         super().__init__()
 
     @staticmethod
-    def to_dict(argumentation_framework: AbstractArgumentationFramework):
-        return {'name': argumentation_framework.name,
-                'arguments':
-                    [str(argument)
-                     for argument in argumentation_framework.arguments],
-                'defeats': [(str(defeat.from_argument),
-                             str(defeat.to_argument))
-                            for defeat in argumentation_framework.defeats]}
+    def to_dict(af: AbstractArgumentationFramework) -> dict:
+        # 1) Name
+        result = {'name': af.name}
 
-    def write(self, argumentation_framework: AbstractArgumentationFramework,
+        # 2) Arguments → list of {id, annotation, url}
+        args = []
+        for arg in af.arguments:
+            args.append({
+                'id':         getattr(arg, 'id', str(arg)),
+                'annotation': getattr(arg, 'annotation', ""),
+                'url':        getattr(arg, 'url', "")
+            })
+        result['arguments'] = args
+
+        # 3) Defeats → list of {from, to, annotation}
+        defs_ = []
+        for d in af.defeats:
+            attacker = d.from_argument
+            target   = d.to_argument
+            defs_.append({
+                'from':       getattr(attacker, 'id', str(attacker)),
+                'to':         getattr(target,   'id', str(target)),
+                'annotation': getattr(d, 'annotation', "")
+            })
+        result['defeats'] = defs_
+
+        return result
+
+    def write(self,
+              argumentation_framework: AbstractArgumentationFramework,
               file_name: str):
         write_path = self.data_folder / file_name
         result = self.to_dict(argumentation_framework)
-        with open(write_path, 'w') as write_file:
-            json.dump(result, write_file)
+        # pretty‑print JSON with 2‑space indent
+        with open(write_path, 'w', encoding='utf-8') as write_file:
+            json.dump(result, write_file, indent=2, ensure_ascii=False)
